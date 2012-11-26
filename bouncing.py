fb0cb7b7c0f61a27f87f20e60bd4671de823c956
earthling.shruti <earthling.shruti@gmail.com>
2012-11-26 14:00:22 -0700

    updated start coordinates and velocity variable name

--- bouncing.py
+++ bouncing.py
@@ -5,23 +5,23 @@ import time
 class Ball():
 
   def __init__(self):
-    self.x = random.randrange(20, size[0]+1)
-    self.y = random.randrange(20, size[1]+1)
-    self.color = [random.randrange(255) for i in range(3)]
     self.size = random.randrange(8, 20)
-    self.change_x = random.randrange(-10, 11, 1)/10.0
-    self.change_y = random.randrange(-10, 11, 1)/10.0
+    self.x = random.randrange(20, size[0]-self.size)
+    self.y = random.randrange(20, size[1]-self.size)
+    self.color = [random.randrange(255) for i in range(3)]
+    self.velocity_x = random.randrange(-10, 11, 1)/10.0
+    self.velocity_y = random.randrange(-10, 11, 1)/10.0
     time.sleep(0.0004)
 
   def move(self):
-    if self.change_x == 0 and self.change_y == 0:
-      self.change_x = 1
+    if self.velocity_x == 0 and self.velocity_y == 0:
+      self.velocity_x = 1
     if self.x >= (size[0]-self.size) or self.x <= self.size:
-      self.change_x = -self.change_x
+      self.velocity_x = -self.velocity_x
     if self.y >= (size[1]-self.size) or self.y <= self.size:
-      self.change_y = -self.change_y
-    self.x += self.change_x
-    self.y += self.change_y
+      self.velocity_y = -self.velocity_y
+    self.x += self.velocity_x
+    self.y += self.velocity_y
 
   def draw(self, screen):
     pygame.draw.circle(screen, self.color, [int(self.x), int(self.y)], self.size)
